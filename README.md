3d6296dd8ea8770cf491d0a8a66b8e9fed2a4c47
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2021-06-05 17:01:44 -0700

    Functionality: Connects to WSDOT API, hits API every 60 seconds.

---
+++ README.md
@@ -0,0 +1,3 @@
+Welcome!
+
+This bot checks the Washington State Department of Transportation every 60 seconds to determine if there is a bridge lift on the Interstate 5 Bridge that connects the states of Oregon and Washington.
---
+++ server.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const axios = require('axios')
+const fetch = require("node-fetch");
+require('dotenv').config();
+const { resolve } = require('path');
+const accessCode = process.env.ACCESS_CODE;
+const url = "http://www.wsdot.wa.gov/Traffic/api/HighwayAlerts/HighwayAlertsREST.svc/GetAlertsAsJson?AccessCode="+accessCode;
+
+const port = "3000"
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end('<h1>Hello, World!</h1>')
+})
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+  
+  setInterval (apiCall, 60000);
+
+
+
+
+})
+
+const apiCall = () => {   
+  axios.get(url)
+  .then(res => {
+    const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+    console.log('Status Code:', res.status);
+    console.log('Date in Response header:', headerDate);
+
+    const filteredAlerts = res.data.filter(({Region}) => {
+      return Region === "Southwest"
+    }).filter(({EventCategory}) => {
+      return EventCategory === "Bridge Lift"
+    });
+    console.log(filteredAlerts);
+
+    if (filteredAlerts.length >= 1) {
+      console.log("Bridge is up.")
+    } else {
+      console.log("Bridge is down. Traffic is flowing.")
+    }
+
+    return filteredAlerts
+  
+  })
+  .catch(err => {
+    console.log('Error: ', err.message);
+  });
+}
